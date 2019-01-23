52725ae31265c8486a0af7bf7672342e0c00d86c
Stephan-kashkarov <Stephankashkarovofficial@gmail.com>
2019-01-23 20:52:31 +1100

    thought about mvc

---
+++ app/controllers/todo.controller.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const MongoClient = require("mongodb").MongoClient
+const mongoose = require('mongoose')
+
+
+let controller = {
+    queryDatabase: function (req, res) {
+
+    },
+    updateDatabase: function (req, res) {
+    
+    }
+}
+module.exports = controller
